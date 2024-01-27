bced96e02f6219dbeac6381f32e4352e9e3ea007
Ali Hojaji <140146049+AliHojaji@users.noreply.github.com>
2024-01-27 14:05:51 +0330

    Add files via upload

---
+++ 2-Upgrade and Migrate to Windows Server 2016.ps1
@@ -0,0 +1,27 @@
+﻿#--- Author : Ali Hojaji ---#
+
+#--*-----------------------*--#
+#---> Upgrade & Migrate 2 <---#
+#--*-----------------------*--#
+
+#--> install the .NET 3.5 framework from the sxs store on the installation media
+Get-WindowsFeature net-framework-Core
+Install-WindowsFeature net-framework-Core -Source D:\sources\sxs
+
+#--> register the smt tools
+.\SMT_WS12R2_amd64\SmigDeploy.exe
+
+#--> import smig cmdlets into session
+Add-PSSnapin Microsoft.windows.ServerManager.Migration
+
+#--> stop the DHCP server service
+Stop-Service -Name DHCPServer
+
+#--> view roles and features eligible for migration
+Get-SmigServerFeature
+
+#--> create the migration store
+Export-SmigServerSetting -FeatureID DHCP -Path c:\Test\migration -Verbose
+
+#--> copy migration store to destination server
+copy_Item -Path c:\Test\migration -Destination \\srv2016-nug\c$\Test -Recurse
