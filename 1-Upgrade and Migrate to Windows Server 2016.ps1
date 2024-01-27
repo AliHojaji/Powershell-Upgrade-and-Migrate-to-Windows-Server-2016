eea674d77cf80bd469a7c1ab5a032d7fecb9cb91
Ali Hojaji <140146049+AliHojaji@users.noreply.github.com>
2024-01-27 14:05:20 +0330

    Add files via upload

---
+++ 1-Upgrade and Migrate to Windows Server 2016.ps1
@@ -0,0 +1,23 @@
+﻿#--- Author : Ali Hojaji ---#
+
+#--*---------------------*--#
+#---> Upgrade & Migrate <---#
+#--*---------------------*--#
+
+#--> view server migration tools feature
+Get-WindowsFeature -Name migration
+
+#--> install windows server migration tools
+Install-WindowsFeature -Name migration
+
+#--> navigate to smt directory
+cd C:\Windows\System32\servermigrationtools
+
+#--> generate package for source machine
+.\smigdeploy.exe /package /architecture amd64 /os WS12R2 /path c:\Test
+
+#--> copy package over to source machine
+Copy-Item -Path c:\Test\SMT_ws12R2_amd64 -Destination \\srv2012-nug\c$\Test
+
+
+#--> complete steps on SRV2012-Test (source machine) before going further <--#
